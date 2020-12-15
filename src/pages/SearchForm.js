fb6d42aff2f5dd09ca6070990600d847d0f200fa
Garrett Hudson <garretthudson87@yahoo.com>
2020-12-15 16:09:49 -0500

    added trim and lowercase to search values to make it easier to match

--- src/pages/SearchForm.js
+++ src/pages/SearchForm.js
@@ -8,7 +8,7 @@ function SearchForm() {
     const [searchValue, setSearchValue] = useState('');
     const [loading, setLoading] = useState(true);
     const [starWarsData, setStarWarsData] = useState([]);
-    const [finalValue, setFinalValue] = useState('');
+    const [characterToDisplay, setCharacterToDisplay] = useState('');
 
     const initialUrl = 'https://akabab.github.io/starwars-api/api';
 
@@ -30,14 +30,14 @@ function SearchForm() {
       }
 
     function handleChange(event) {
-        setFinalValue('');
+        setCharacterToDisplay('');
         setSearchValue(event.target.value);
     }
 
 
     function handleSubmit(event) {
         event.preventDefault();
-        setFinalValue(searchValue);
+        setCharacterToDisplay(searchValue.trim().toLowerCase());
         setSearchValue('');
     }
 
@@ -49,7 +49,7 @@ function SearchForm() {
             <input style={{width: '60%', alignItems: 'center', height: '2rem', borderRadius: '5px'}} type='text' value={searchValue} onChange={handleChange} />
             <button style={{backgroundColor: '#f9d71c', borderRadius: '5px', color: 'black', fontWeight: '800', marginTop: '2rem', border: '1px solid black', cursor: 'pointer', padding: '.5rem'}} onClick={handleSubmit}>SEARCH</button>
             {starWarsData.map((character, i) => {
-            if (character.name === finalValue) {
+            if (character.name.trim().toLowerCase() === characterToDisplay) {
                 return <Card key={i} character={character} />
             }
               return null;
