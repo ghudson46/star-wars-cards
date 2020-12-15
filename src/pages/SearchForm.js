f99582fd78160a25b173493b5a0c1a15bc7729b4
Garrett Hudson <garretthudson87@yahoo.com>
2020-12-15 15:48:14 -0500

    finished search feature

--- src/pages/SearchForm.js
+++ src/pages/SearchForm.js
@@ -2,10 +2,13 @@ import React, { useState, useEffect } from 'react';
 
 import { getCharacter, getAllCharacters } from '../Services/characters';
 
+import Card from '../components/Card/Card';
+
 function SearchForm() {
     const [searchValue, setSearchValue] = useState('');
     const [loading, setLoading] = useState(true);
     const [starWarsData, setStarWarsData] = useState([]);
+    const [finalValue, setFinalValue] = useState('');
 
     const initialUrl = 'https://akabab.github.io/starwars-api/api';
 
@@ -27,14 +30,14 @@ function SearchForm() {
       }
 
     function handleChange(event) {
+        setFinalValue('');
         setSearchValue(event.target.value);
     }
 
+
     function handleSubmit(event) {
         event.preventDefault();
-        
-        getCharacter(initialUrl + '/id/1.json');
-
+        setFinalValue(searchValue);
         setSearchValue('');
     }
 
@@ -43,10 +46,14 @@ function SearchForm() {
     return (
         <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '3rem'}}>
             <label htmlFor='search' style={{color: '#f9d71c', fontSize: '1.5rem', fontWeight: '600', marginBottom: '1rem'}}>Search for a character by name</label>
-            <input style={{width: '60%', alignItems: 'center'}} type='text' value={searchValue} onChange={handleChange} />
-            <button onClick={handleSubmit}>SEARCH</button>
-
-            <Card key='1' character={character} />
+            <input style={{width: '60%', alignItems: 'center', height: '2rem', borderRadius: '5px'}} type='text' value={searchValue} onChange={handleChange} />
+            <button style={{backgroundColor: '#f9d71c', borderRadius: '5px', color: 'black', fontWeight: '800', marginTop: '2rem', border: '1px solid black', cursor: 'pointer', padding: '.5rem'}} onClick={handleSubmit}>SEARCH</button>
+            {starWarsData.map((character, i) => {
+            if (character.name === finalValue) {
+                return <Card key={i} character={character} />
+            }
+              return null;
+            })}
         </div>
     )
 }
