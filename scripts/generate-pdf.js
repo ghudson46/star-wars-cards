bf2508499bcf4b5d1ffff65d5ef020a31dd28575
Garrett Hudson <garretthudson87@yahoo.com>
2021-01-04 10:33:44 -0500

    starting to add pdf exports to the cards

--- scripts/generate-pdf.js
+++ scripts/generate-pdf.js
@@ -3,5 +3,13 @@ const puppeteer = require('puppeteer');
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('http://localhost:3000/', {waitUntil: 'netowrkidle2'});
+    await page.goto('http://localhost:3000/');
+    await page.emulateMedia('screen');
+    await page.pdf({
+        path: '/react.pdf', // Path (relative to CWD) to save the PDF to
+        printBackground: true, // print background colors
+        width: '612px', // Match the css width and height set for PDF
+        height: '792px',
+    });
+    await browser.close();
 })()
